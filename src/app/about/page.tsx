8b99b329dba372c309879dbf4b1e0fdc6bf94504
Breno-Mateus <brenomateus36@gmail.com>
2025-03-28 15:52:03 -0300

    feat: add page contact

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,8 +9,8 @@ export default function About() {
         </h1>
         <div className="flex flex-col gap-4 text-text-typography font-light text-base">
           <p>
-            Olá! Sou Sandro Luz, apaixonado pela arte que transcende o tempo e se
-            imprime na pele. Com mais de 10 anos de experiência no mundo da
+            Olá! Sou Sandro Luz, apaixonado pela arte que transcende o tempo e
+            se imprime na pele. Com mais de 10 anos de experiência no mundo da
             tatuagem, dedico-me a transformar visões e emoções em arte corporal
             que expressa quem você é e o que você ama.
           </p>
@@ -19,18 +19,21 @@ export default function About() {
             atentamente os desejos e as histórias de meus clientes para que cada
             peça seja única e pessoal. Especializo-me em vários estilos, desde o
             delicado traço fino até composições mais robustas e coloridas,
-            garantindo que cada design não só atenda, mas supere as expectativas.
+            garantindo que cada design não só atenda, mas supere as
+            expectativas.
           </p>
           <p>
-            Além da dedicação à arte, a segurança e o conforto são prioridades no
-            meu estúdio. Utilizo apenas equipamentos esterilizados e de alta
+            Além da dedicação à arte, a segurança e o conforto são prioridades
+            no meu estúdio. Utilizo apenas equipamentos esterilizados e de alta
             qualidade, e sou rigoroso quanto aos protocolos de higiene e
-            segurança, garantindo uma experiência segura e confortável para todos.
+            segurança, garantindo uma experiência segura e confortável para
+            todos.
           </p>
           <p>
-            Visite meu estúdio e veja como a arte pode se manifestar em sua pele.
-            Estou aqui para ajudá-lo a contar sua história de uma maneira única e
-            eterna. Agende sua consulta e vamos juntos criar algo extraordinário!
+            Visite meu estúdio e veja como a arte pode se manifestar em sua
+            pele. Estou aqui para ajudá-lo a contar sua história de uma maneira
+            única e eterna. Agende sua consulta e vamos juntos criar algo
+            extraordinário!
           </p>
         </div>
         <p className="font-island text-4xl">Sandro Luz</p>
